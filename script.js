7f0a18b1b7204afc3ef69d245186cf8170ae068a
andres <jhonandres4360435@gmail.com>
2024-12-21 16:28:27 -0500

    subiendo archivos

---
+++ script.js
@@ -0,0 +1,10 @@
+let nombre="andres";
+let apellido="calderon"
+let edad=28;
+const estudiantes=["mateo","pedro","jeison"];
+const materias={
+    matematicas:5.0,
+    fisica:4.0
+}
+
+console.table(materias);
